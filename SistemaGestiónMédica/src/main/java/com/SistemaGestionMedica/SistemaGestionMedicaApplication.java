6b8049330bc35908e85fa88f544e7034c25582bb
David <shirigami31@gmail.com>
2025-08-06 19:22:38 -0500

    iniciando commit

---
+++ SistemaGestiónMédica/src/main/java/com/SistemaGestionMedica/SistemaGestionMedicaApplication.java
@@ -0,0 +1,14 @@
+package com.SistemaGestionMedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionMedicaApplication.class, args);
+	}
+
+}
+
